32f3e318a24acb195562691730bc7ad5f6227a0c
Hp <adkhamjon.abd@gmail.com>
2024-12-24 16:57:49 +0500

    Makes changes to pom.xml to fix the problem with lombok

--- src/main/java/com/example/rest_service/Employee.java
+++ src/main/java/com/example/rest_service/Employee.java
@@ -3,9 +3,11 @@ package com.example.rest_service;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
     //Step 3: Create a resource class for the Employee that includes private variables for
     // employee_id, first_name, last_name, email, and title. Also include getter functions
